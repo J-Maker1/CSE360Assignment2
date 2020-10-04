de8861dcd8fd78ceeb141499e44ab67cec988af6
Justin <Justin@DESKTOP-9CKK1ME>
2020-10-03 20:29:46 -0700

    Initial Creation of Adding Machine Class

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  /*
+   * AddingMachine will define all the functions for manipulating a value
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*
+   * @return should return the current total 
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /*
+   * @param value the integer that will be added to the total variable
+   */
+  public void add (int value) {
+  }
+  /*
+   * @param value the integer that will be subtracted from the total variable
+   */
+  public void subtract (int value) {
+  }
+  /*
+   * @return should return the order of operations
+   */
+  public String toString () {
+    return "";
+  }
+  /*
+   * This method should clear the memory
+   */
+  public void clear() {
+  }
+}
