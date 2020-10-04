097f9efe7f4a51aaf329e50a63025172a1767d6f
Justin <Justin@DESKTOP-9CKK1ME>
2020-10-03 21:50:07 -0700

    Added comments to the test class

--- cse360assignment02/src/cse360assignment02/Tester.java
+++ cse360assignment02/src/cse360assignment02/Tester.java
@@ -1,14 +1,17 @@
 package cse360assignment02;
-
+/*
+ * This class is meant only for testing AddingMachine to make sure it works as intended.
+ * Please refer to @AddingMAchine for the assignment
+ */
 public class Tester {
 	public static void main(String[] args) {
-		AddingMachine myCalculator = new AddingMachine();
+		AddingMachine myCalculator = new AddingMachine();	//create object out of AddingMachine
 		myCalculator.add(4);
-		myCalculator.subtract(2);
+		myCalculator.subtract(2);	//Calling the add and subtract functions
 		myCalculator.add(5);
-		System.out.println(myCalculator.toString());
-		System.out.println(myCalculator.getTotal());
-		myCalculator.clear();
+		System.out.println(myCalculator.toString());	//Testing toString
+		System.out.println(myCalculator.getTotal());	//Testing getTotal
+		myCalculator.clear();							//Testing clear
 		System.out.println(myCalculator.toString());
 		System.out.println(myCalculator.getTotal());
 	}
