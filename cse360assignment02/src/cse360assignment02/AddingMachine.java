3638ea8b88a24a44a35be4a09f0816f1469d9f90
Justin <Justin@DESKTOP-9CKK1ME>
2020-10-03 21:35:27 -0700

    Added lines to build the string that toString returns, and completed the clear function.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -2,11 +2,13 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String str;
   /*
-   * AddingMachine will define all the functions for manipulating a value
+   * Constructor for AddingMachine that will initialize variables
    */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    str = "0";
   }
   /*
    * @return should return the current total 
@@ -19,22 +21,26 @@ public class AddingMachine {
    */
   public void add (int value) {
 	  total += value;
+	  str += " + " + total;
   }
   /*
    * @param value the integer that will be subtracted from the total variable
    */
   public void subtract (int value) {
 	  total -= value;
+	  str += " - " + total;
   }
   /*
    * @return should return the order of operations
    */
   public String toString () {
-    return "";
+    return str + "\n";
   }
   /*
    * This method should clear the memory
    */
   public void clear() {
+	  total = 0;
+	  str = "0";
   }
 }
